231a9451b492fa0856b9e7ad3d0f40456451633b
TheCodingZone <114797688+TheCodingZone@users.noreply.github.com>
2023-11-28 22:49:45 +0530

    Add files via upload

---
+++ Developer.js
@@ -0,0 +1,14 @@
+import Navbar from '@/pages/Navbar/Navbar'
+import React from 'react'
+
+const Developer = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h3> Welcome! Jhon</h3>
+     
+    </div>
+  )
+}
+
+export default Developer
